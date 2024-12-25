0e150760361527020b6ea71935aa1c1f23274756
highfrezh <highfrezh14@gmail.com>
2024-12-25 14:51:50 +0100

    data ingestion added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "end-to-end-brain-tumor-classification-using-MRI-Dataset"
+AUTHOR_USER_NAME = "highfrezh"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "olabintanibraheem@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Simple python package for CNN Classification Project",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
